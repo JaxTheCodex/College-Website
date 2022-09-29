e7c7f4cd84004660d56e4cebdbe85047eaf8209d
JaxTheCodex <jagarnathpatro836@gmail.com>
2022-09-29 09:52:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayDate(){
+    document.getElementById("showDate").innerHTML=Date();
+}
+function sub(){
+  alert("you are logined to lit portal");
+}
+function su(){
+  window.alert("keep your registration form with you")
+} 
